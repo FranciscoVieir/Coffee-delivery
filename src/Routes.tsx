2413e3ec8beb1d6f5dc552858ade678ee23bfa20
FranciscoVieir <franciscovieira6397@gmail.com>
2023-02-18 16:40:36 -0300

    feat: Adding "<outlet />" from react dom to render header component on all routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayouts'
 import { CompleteOrderPage } from './pages/CompleteOrder'
 import { HomePage } from './pages/Home'
 
 export function Router() {
   return (
     <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/completeorder' element={<CompleteOrderPage />} />
+      <Route path='/' element={<DefaultLayout />}>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/completeorder' element={<CompleteOrderPage />} />
+      </Route>
     </Routes>
   )
 }
