b693a4a92384f756d1c6b3dd6d966c9d9cc868d7
FranciscoVieir <franciscovieira6397@gmail.com>
2023-02-18 17:21:09 -0300

    feat: creating and customizing the component header

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,25 @@
+import { HeaderButton, HeaderButtonsContainer, HeaderContainer } from "./styles";
+import coffeeLogoDelivery from '../../assets/Logo.png'
+import { MapPin, ShoppingCart } from 'phosphor-react'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <div className="container">
+        <img src={coffeeLogoDelivery} alt="coffee logo" />
+
+        <HeaderButtonsContainer>
+
+          <HeaderButton variant="purple">
+            <MapPin size={20} weight="fill" />
+            Porto Alegre, RS
+          </HeaderButton>
+
+          <HeaderButton variant="yellow">
+            <ShoppingCart size={20} weight="fill" />
+          </HeaderButton>
+        </HeaderButtonsContainer>
+      </div>
+    </HeaderContainer>
+  )
+}
---
+++ src/pages/components/menuIntro/index.tsx
@@ -0,0 +1,7 @@
+export function menuIntroPage() {
+  return (
+    <div>
+      <h3>Introdução</h3>
+    </div>
+  )
+}
