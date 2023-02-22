911655d1c55469efdefd1d6209ade1d4ad17c8f7
FranciscoVieir <franciscovieira6397@gmail.com>
2023-02-22 14:59:55 -0300

    feat: adding a product counter to the shopping cart

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -2,8 +2,11 @@ import { HeaderButton, HeaderButtonsContainer, HeaderContainer } from "./styles"
 import coffeeLogoDelivery from '../../assets/Logo.png'
 import { MapPin, ShoppingCart } from 'phosphor-react'
 import { NavLink } from "react-router-dom";
+import { useCart } from "../../hooks/useCart";
 
 export function Header() {
+  const { cartQuantity } = useCart();
+
   return (
     <HeaderContainer>
       <div className="container">
@@ -20,6 +23,7 @@ export function Header() {
 
           <NavLink to="/completeorder">
             <HeaderButton variant="yellow">
+              {cartQuantity >= 1 && <span>{cartQuantity}</span>}
               <ShoppingCart size={20} weight="fill" />
             </HeaderButton>
           </NavLink>
--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -8,7 +8,8 @@ export interface CartItem extends Coffee {
 
 interface CartContextType {
   cartItems: CartItem[];
-  addCoffeeToCart: (Coffee: CartItem) => void
+  cartQuantity: number;
+  addCoffeeToCart: (Coffee: CartItem) => void;
 }
 
 interface CartContextProviderProps {
@@ -19,6 +20,8 @@ export const CartContext = createContext({} as CartContextType);
 
 export function CartContextProvider({ children }: CartContextProviderProps) {
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const cartQuantity = cartItems.length
 
   function addCoffeeToCart(coffee: CartItem) {
     const coffeeAlreadyExistsInCart = cartItems.findIndex((cartItem) => cartItem.id === coffee.id)
@@ -37,7 +40,7 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
   console.log(cartItems)
 
   return (
-    <CartContext.Provider value={{ cartItems, addCoffeeToCart }}>
+    <CartContext.Provider value={{ cartItems, cartQuantity, addCoffeeToCart }}>
       {children}
     </CartContext.Provider>
   )
